fc4e0e9c3f094a87488bf2f6bfc6e5e57a272c4c
= <d.sek464@gmail.com>
2022-04-21 20:07:06 +0200

    fixed heatmap

--- src/components/HeatMap/HeatMap.js
+++ src/components/HeatMap/HeatMap.js
@@ -4,26 +4,25 @@ import { scaleLinear } from "d3-scale";
 import WorldMap from "react-svg-worldmap";
 
 const HeatMap = ({flights}) => {
+    const [data, setData] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
 
-    const parsedData = [
-        { country: "cn", value: 1 }, // china
-    ]
-   const [isLoaded, setIsLoaded] = useState(false);
         useEffect(() => {
                 fetch('https://gothic459.pythonanywhere.com/api/flights/airspaces')
                     .then(response => response.json())
-                    .then(data => {
-                        Object.keys(data).map(function(key, index) {
-                            parsedData.push({
-                                country: key.toLowerCase(),
-                                value: data[key].amount
-                            });
+                    .then(responseJson => {
+                        Object.keys(responseJson).forEach(key => {
+                            setData(data => [...data, {
+                                "country": key,
+                                value: responseJson[key].amount
+                            }]);
                         });
-                        console.log(parsedData);
                         setIsLoaded(true);
-
                     });
         }, []);
+
+        console.log(data)
+        
    return (
        <>
            {isLoaded ? <WorldMap color="green"
@@ -31,7 +30,7 @@ const HeatMap = ({flights}) => {
                                   frame="true"
                                   title="Heat Map"
                                   size="lg"
-                                  data={parsedData} /> :
+                                  data={data} /> :
                <div>Loading...</div>}
        </>
    );
